d908d901465184a7f21207b70380f0c5a72fd115
MegaChie <workspace.mega@gmail.com>
2023-04-21 22:14:50 +0200

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 --- comment text
-- host: all
-  become: true
-  vars:
-    package: flask
-  tasks:
-    -name: install flask package
-    apt: name={{ package }} state=latest
+$filePlace = "/tmp/school"
+file { $filePlace:
+	ensure => "directory",
+	owner => "www-data",
+	group => "www-data",
+	content => "I love Puppet",
+	mode => 0744
+}
