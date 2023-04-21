642b8fdf4f53716a05acbac38e925f8652d62fea
MegaChie <workspace.mega@gmail.com>
2023-04-21 22:22:17 +0200

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 --- comment text
-$file_place = "/tmp/school"
+$file_place = '/tmp/school'
 file { $file_place:
-    ensure => "directory",
-    owner => "www-data",
-    group => "www-data",
+    ensure  => 'directory',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => "I love Puppet",
-    mode => 744
+    mode    => 744
 }
