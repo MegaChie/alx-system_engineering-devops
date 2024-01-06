76cf3428961182f165bdfcd88f4caeab8c57fbc5
MegaChie <workspace.mega@gmail.com>
2024-01-06 01:49:42 -0800

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,11 +8,9 @@ import sys
 def API():
     """Get user name and undone tasks list"""
     # getting user name
-    oldUrl = "https://jsonplaceholder.typicode.com/users/"
-    url = oldUrl + sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/"
     with requests.get(url) as marko:
         polo = marko.json()
-        name = polo['name']
         print(type(polo))
         print(len(polo))
 
