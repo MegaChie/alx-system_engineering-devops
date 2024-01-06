ccc227268ec19fd8aedee26ac4f59c753668588f
MegaChie <workspace.mega@gmail.com>
2024-01-06 20:29:22 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -27,9 +27,9 @@ def API():
                     # line = {ID: [{"task": elem["title"],
                     #         "completed": elem["completed"],
                     #         "username": name}]}
-                    file.write(json.dumps(ID: [{"task": elem["title"],
+                    file.write(json.dumps("ID: [{"task": elem["title"],
                             "completed": elem["completed"],
-                            "username": name}]))
+                            "username": name}])")
 
 
 if __name__ == "__main__":
