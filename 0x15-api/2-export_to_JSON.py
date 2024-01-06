961f05a9868e90ddf6e6563aaca4d4d95c19bd98
MegaChie <workspace.mega@gmail.com>
2024-01-06 22:08:26 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,22 +13,20 @@ def API():
     tasksUrl = baseUrl + "todos"
     with requests.get(usersUrl) as marko:
         polo = marko.json()
-        ID = polo["id"]
         name = polo["username"]
 
     # Getting tasks list
     with requests.get(tasksUrl) as marko:
         polo = marko.json()
         fileName = sys.argv[1] + ".json"
+        file.write("{{}: ".format(int(sys.argv[1])))
         with open(fileName, "a", encoding="utf-8") as file:
             for elem in polo:
                 if elem["userId"] == int(sys.argv[1]):
-                    line = json.dumps([{"task": elem["title"],
-                        "completed": elem["completed"], "username": name}])
-                    print(line)
-                    # file.write(json.dump({ID: [{"task": elem["title"],
-                    #     "completed": elem["completed"], "username": name}]}))
-                    
+                    file.write(json.dumps([{"task": elem["title"],
+                        "completed": elem["completed"], "username": name}]),
+                        separators=(',',))
+        file.write("}")
 
 
 if __name__ == "__main__":
