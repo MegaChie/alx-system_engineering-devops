a78ed9906d3b0707afd41f0133da0cbb7a195a0f
MegaChie <workspace.mega@gmail.com>
2024-01-06 11:15:12 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-import sys
-import urllib
-
 """API usage"""
-def API():
-	"""Get user name and undone tasks list"""
-	# getting user name
-	oldUrl = "https://jsonplaceholder.typicode.com/users/"
-	url = oldUrl + sys.argv[1]
-	print(url)
+import json
+import requests
+import sys
 
+
+def API():
+    """Get user name and undone tasks list"""
+    # getting user name
+    oldUrl = "https://jsonplaceholder.typicode.com/users/"
+    url = oldUrl + sys.argv[1]
+    with requests.get(url) as data:
+        print(data)
 
 
 if __name__ == "__main__":
-	API()
+    API()
