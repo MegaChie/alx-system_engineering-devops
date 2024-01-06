4cea76e3a21866afa55ff32059323d430d3b213d
MegaChie <workspace.mega@gmail.com>
2024-01-06 14:06:26 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -24,8 +24,7 @@ def API():
         for elem in polo:
             if elem["userId"] == int(sys.argv[1]):
                 tasks = tasks + 1
-            if elem["userId"] == int(sys.argv[1]) and
-            elem["completed"] == "true":
+            if elem["userId"] == int(sys.argv[1]) and elem["completed"] is "true":
                 done = done + 1
         print("{}/{}".format(done, tasks))
 
