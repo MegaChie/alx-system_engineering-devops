2a2a9e2176dcabfe974e123134350e0accd909a6
MegaChie <workspace.mega@gmail.com>
2024-01-06 22:03:16 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -23,8 +23,8 @@ def API():
         with open(fileName, "a", encoding="utf-8") as file:
             for elem in polo:
                 if elem["userId"] == int(sys.argv[1]):
-                    line = json.dumps({ID: [{"task": elem["title"],
-                        "completed": elem["completed"], "username": name}]})
+                    line = json.dumps([{"task": elem["title"],
+                        "completed": elem["completed"], "username": name}])
                     print(line)
                     # file.write(json.dump({ID: [{"task": elem["title"],
                     #     "completed": elem["completed"], "username": name}]}))
