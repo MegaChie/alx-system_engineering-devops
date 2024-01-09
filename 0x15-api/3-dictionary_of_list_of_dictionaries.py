340df91075d508de6a0365ca49d71dd881819fbe
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-09 12:01:07 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,30 +9,27 @@ def API():
     """Build a JSON file with id, username, task, and task status"""
     # getting user name
     baseUrl = "https://jsonplaceholder.typicode.com/"
-    usersUrl = baseUrl + "users/" + sys.argv[1]
     tasksUrl = baseUrl + "todos"
-    with requests.get(usersUrl) as marko:
-        polo = marko.json()
-        name = polo["username"]
+    def empnam(a):
+        usersUrl = baseUrl + "users/" + a
+        with requests.get(usersUrl) as marko:
+            polo = marko.json()
+            name = polo["username"]
+            return name
 
     # Getting tasks list
     with requests.get(tasksUrl) as marko:
         polo = marko.json()
-        fileName = sys.argv[1] + ".json"
+        undone = []
+        fileName = "todo_all_employees.json"
+        for user in polo:
+            tasks = []
+            if polo["completed"] is False:
+                tasks.append(json.dump(polo["userId"]: [{
+                    "username": empnam(polo["userId"]),
+                    "task": polo["title"],
+                    "completed": polo["completed"]}]))
         with open(fileName, "a", encoding="utf-8") as file:
-            items = []
-            file.write("{\"")
-            file.write(str(sys.argv[1]))
-            file.write("\": [")
-            for elem in polo:
-                if elem["userId"] == int(sys.argv[1]):
-                    items.append(json.dumps({
-                        "task": elem["title"],
-                        "completed": elem["completed"],
-                        "username": name})
-                    )
-            file.write(", ".join(items))
-            file.write("]}")
 
 
 if __name__ == "__main__":
