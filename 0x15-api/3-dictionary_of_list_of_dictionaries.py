4957d3de527263f4005434c0fc627bb201a455ac
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-09 18:40:10 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -27,7 +27,7 @@ def API():
                 if user["completed"] is False and user["userId"] == num:
                     undone.append({"username": emna(num),
                                    "task": user["title"],
-                                   "completed": user["completed"]})
+                                   "completed": get(user["completed"])})
             userdic[str(num)] = undone
     print(userdic)
 
