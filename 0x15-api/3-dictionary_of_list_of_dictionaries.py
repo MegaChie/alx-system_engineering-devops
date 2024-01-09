e714537160b994d4a5a68fd152eb740890d2d196
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-09 21:45:56 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -34,7 +34,7 @@ def API():
     # Writing file
     fileName = "todo_all_employees.json"
     with open(fileName, "a", encoding="utf-8") as file:
-        file.write(json.dumps(userdic))
+        json.dump(userdic, file)
 
 
 if __name__ == "__main__":
