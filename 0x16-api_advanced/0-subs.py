4e4cc9fe102306cc08b6bbc0520febb85a77660a
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-27 16:18:21 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,6 +11,5 @@ def number_of_subscribers(subreddit=sys.argv[1]):
     or 0 if an invalid subreddit is given.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    with requests.get(url, headers={"User-Agent": "My-User-Agent"})as marko:
-        polo = str(marko.json())
-    return (polo)
+    polo = requests.get(url, headers={"User-Agent": "My-User-Agent"})
+    return (polo.json())
