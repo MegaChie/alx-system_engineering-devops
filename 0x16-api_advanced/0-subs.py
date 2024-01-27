d65671ca258b088fa0f38aa9af664fbf84b8237d
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-27 14:47:20 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,3 +10,9 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit,
     or 0 if an invalid subreddit is given.
     """
+    subreddit = sys.argv[1]
+    url = "https://www.reddit.com/dev/api//r/{}/about".format(subreddit)
+    # with requests.get(url) as marko:
+    #     polo = marko.json.load()
+    # return (polo)
+    print(url)
