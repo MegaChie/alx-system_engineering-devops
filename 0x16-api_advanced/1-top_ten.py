53404a7d92fa7c07cdb0cb408ca488f781bf0611
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-29 21:50:06 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,16 +16,18 @@ def number_of_subscribers(subreddit=sys.argv[1]):
     login = requests.auth.HTTPBasicAuth(client, clientKey)
     header = {"User-Agent": "Mega/0.0.2"}
     sendData = {"grant_type": "client_credentials"}
+    wanted = {"limit": 9}
     with requests.post("https://www.reddit.com/api/v1/access_token",
-                       auth=login, headers=header,
-                       data=sendData) as authMarko:
+                    auth=login, headers=header,
+                    data=sendData) as authMarko:
         poloKey = authMarko.json()
     header["Authorization"] = "bearer {}".format(poloKey["access_token"])
 
     # using the token to access the API
-    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
-    with requests.get(url, headers=header) as marko:
+    url = "https://oauth.reddit.com/r/{}/hot".format(subreddit)
+    with requests.get(url, headers=header, params=wanted) as marko:
         if marko.status_code >= 300:
-            return 0
-        polo = marko.json().get("data").get("subscribers")
-    return polo
+            return None
+        polo = marko.json()
+    for hot in polo["data"]["children"]:
+        print(["data"]["title"])
