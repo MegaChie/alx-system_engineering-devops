e127e93a0fb42f2e9f59d537b2f0ad4ef4c99c0e
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-29 11:25:00 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit=sys.argv[1]):
     or 0 if an invalid subreddit is given.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    with requests.get(url, headers={{"User-Agent": "Mega\'s"}}) as mark:
+    with requests.get(url, headers={{"User-Agent": "Mega"}}) as mark:
         if marko.status_code >= 300:
             return 0
         polo = marko.json().get("data").get("subscribers")
