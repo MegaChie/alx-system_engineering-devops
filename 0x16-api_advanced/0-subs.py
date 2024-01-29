d7b334987da2f86b8e2196f00dcbdb34b823217b
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-29 19:06:01 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,7 @@ import sys
 
 
 def number_of_subscribers(subreddit=sys.argv[1]):
-    """
-    Returns the number of subscribers for a given subreddit,
-    or 0 if an invalid subreddit is given
-    """
+    """Returns the number of subscribers for a given subreddit"""
     # firstly, authenticate and get token
     client = "XVngJrvVrijzTzOU09512w"
     clientKey = "h6FGkpwlvaqLY1RjMkr-fn4Yu26SNQ"
