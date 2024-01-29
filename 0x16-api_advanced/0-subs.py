17be8fbe06b6f667bca945f8d45b4b6814cc8e49
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-29 14:55:52 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,19 @@ def number_of_subscribers(subreddit=sys.argv[1]):
     Returns the number of subscribers for a given subreddit,
     or 0 if an invalid subreddit is given.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    with requests.get(url, params=={"client_id": "Mega"}) as marko:
+    # firstly, authenticate and get token
+    client = "WGstrtKlV2DGfioJn-RPXQ"
+    clientKey = "eBZqUUfYtdmeWENV9bZCV7ivx2FVAw"
+    login = requests.auth.HTTPBasicAuth(client, clientKey)
+    header = {"User-Agent": "Mega/0.2"}
+    with requests.post("https://www.reddit.com/api/v1/access_token",
+                       auth=login, headers=header) as authMarko:
+        poloKey = authMarko.json()["access_token"]
+    header["Authorization"] = "bearer {}".format(poloKey)
+
+    # using the token to access the API
+    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+    with requests.get(url, headers=header) as marko:
         if marko.status_code >= 300:
             return 0
         polo = marko.json().get("data").get("subscribers")
