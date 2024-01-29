854b9df422758d53b0ef2b94dd300221e2f88cba
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-29 18:36:03 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,17 @@ def number_of_subscribers(subreddit=sys.argv[1]):
     Returns the number of subscribers for a given subreddit,
     or 0 if an invalid subreddit is given.
     """
+
     # firstly, authenticate and get token
-    client = "WGstrtKlV2DGfioJn-RPXQ"
-    clientKey = "eBZqUUfYtdmeWENV9bZCV7ivx2FVAw"
+    client = "XVngJrvVrijzTzOU09512w"
+    clientKey = "h6FGkpwlvaqLY1RjMkr-fn4Yu26SNQ"
     login = requests.auth.HTTPBasicAuth(client, clientKey)
-    header = {"User-Agent": "Mega/0.2"}
+    header = {"User-Agent": "Mega/0.0.2"}
+    sendData = {"grant_type": "client_credentials"}
     with requests.post("https://www.reddit.com/api/v1/access_token",
-                       auth=login, headers=header) as authMarko:
-        poloKey = authMarko.json()["access_token"]
-    header["Authorization"] = "bearer {}".format(poloKey)
+                        auth=login, headers=header, data=sendData) as authMarko:
+        poloKey = authMarko.json()
+    header["Authorization"] = "bearer {}".format(poloKey["access_token"])
 
     # using the token to access the API
     url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
