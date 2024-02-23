3e1d074fa49e64114c9cd17115849d23340832de
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-23 20:57:02 +0200

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # Installs flask from pip3
-package { 'installing Flask package':
-  ensure   => '2.1.0',
-  provider  => 'pip3',
-  name     => 'Flask',
-  install_options => 'installable'
+exec { 'installing Flask package':
+  command => 'pip3 install Flask == 1.2.0',
+  path    => '/usr/bin/'
 }
