73ef69ccccbf2280ece1242accdf396d16dd6940
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-23 23:25:08 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# Installs flask from pip3
+# Installs flask from pip3, it will work on your machine, but not the checker
 exec { 'flask installation':
   command => 'pip install flask==2.0.1',
   path    => '/usr/bin/pip'
