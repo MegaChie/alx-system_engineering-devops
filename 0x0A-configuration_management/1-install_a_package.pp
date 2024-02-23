81720d5e6f8a6318fefde63588753d1b3240b5eb
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-23 19:23:52 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Installs flask from pip3
-package { 'installing Flask':
+package { 'installing Flask package':
   ensure   => '2.1.0',
   name     => 'Flask',
-  provider => 'pip3'
+  provider  => pip3
 }
