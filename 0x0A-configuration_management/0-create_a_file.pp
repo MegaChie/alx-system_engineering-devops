3de1be5962a9392fd2ecb503b2378d4000c1a38e
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-23 14:01:51 +0200

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Creates a file in /tmp.
 file { 'school':
   ensure  => absent
-  path    => './tmp/'
+  path    => '/tmp/school'
   content => 'I love Puppet'
   group   => 'www-data'
   mode    => '0744'
