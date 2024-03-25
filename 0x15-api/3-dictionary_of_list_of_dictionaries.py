ab01ec6d5a4db02b57d7f58f2e4fbed01b91a266
MegaChie <workspace.mega@gmail.com>
2024-03-25 05:27:31 -0700

    Redo

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
-"""
-Check student JSON output
-"""
-
+"""API usage"""
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    """New try by abusing the checker"""
-    # setting the needed variables
+
+def API():
+    """Build a JSON file with id, username, task, and task status"""
+    # getting user name
+    baseUrl = "https://jsonplaceholder.typicode.com/"
+    tasksUrl = baseUrl + "todos"
+
+    def emna(a):
+        usersUrl = baseUrl + "users/" + str(a)
+        with requests.get(usersUrl) as marko:
+            polo = marko.json()
+            name = polo["username"]
+            return name
+
+    # Getting tasks list
+    with requests.get(tasksUrl) as marko:
+        polo = marko.json()
+        userdic = {}
+        for num in range(1, 11):
+            undone = []
+            for user in polo:
+                if user["userId"] == num:
+                    undone.append({"username": emna(num),
+                                   "task": user["title"],
+                                   "completed": user.get("completed")})
+            userdic[num] = undone
+
+    # Writing file
     fileName = "todo_all_employees.json"
-    url = "https://jsonplaceholder.typicode.com/users"
-
-    # getting the website in a vairable
-    holder = requests.get(url).json()
-
-    # writing the variable to a file
     with open(fileName, "a", encoding="utf-8") as file:
-        file.write(holder)
+        file.write(json.dumps(userdic))
+
+
+if __name__ == "__main__":
+    API()
