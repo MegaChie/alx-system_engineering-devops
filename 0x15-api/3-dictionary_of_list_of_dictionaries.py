4f220d1515ab512ea3abf138b63d84db01998e00
MegaChie <workspace.mega@gmail.com>
2024-03-25 06:53:25 -0700

    Dynamicaly getting the number of employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,22 +7,31 @@ import sys
 
 def API():
     """Build a JSON file with id, username, task, and task status"""
-    # getting user name
+    # Setting URLs
     baseUrl = "https://jsonplaceholder.typicode.com/"
     tasksUrl = baseUrl + "todos"
+    emplUrl = baseUrl + "users"
 
     def emna(a):
+        """Returns the user name from the user URL"""
+        # Setting the URL
         usersUrl = baseUrl + "users/" + str(a)
+        # Fetches the required data and isolates the name
         with requests.get(usersUrl) as marko:
             polo = marko.json()
             name = polo["username"]
             return str(name)
 
+    #Fetchs the number of the users
+    emplNumb = None
+    with requests.get(emplUrl) as marko:
+            polo = marko.json()
+            emplNumb = len(polo)
     # Getting tasks list
     with requests.get(tasksUrl) as marko:
         polo = marko.json()
         userdic = {}
-        for num in range(1, 11):
+        for num in range(1, emplNumb + 1):
             undone = []
             for user in polo:
                 if user["userId"] == num:
