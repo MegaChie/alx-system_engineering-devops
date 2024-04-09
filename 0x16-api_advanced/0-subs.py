bb46f0d5aca0766ba63c7af82798a31e492833cd
MegaChie <workspace.mega@gmail.com>
2024-04-09 04:51:54 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,9 @@
 """API usage"""
 import json
 import requests
-import sys
 
 
-def number_of_subscribers(subreddit=sys.argv[1]):
+def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit,
     or 0 if an invalid subreddit is given.
