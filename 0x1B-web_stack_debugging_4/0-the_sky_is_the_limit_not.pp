805fc7657117f45f82a2c5b0b79aeb3fa1a735d2
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-04-18 17:02:54 +0200

    Fix typo in the comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Increase the request limit of Nginx
+# increace the request limit of Nginx
 exec { 'Change Nginx limit':
   command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
   provider => shell,
